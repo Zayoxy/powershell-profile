d5bedfa6edabb04922c21a0403604f56a0464065
Alexandre G <zayoxy@pm.me>
2023-02-26 04:17:03 +0100

    Setup script fixed

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,6 @@
 $VerbosePreference = 'Continue'
+$profileFile = "Microsoft.PowerShell_profile.ps1"
+Clear-Host
 
 # Handling Profile creation
 try
@@ -8,21 +10,65 @@ try
     if(Test-Path -Path $PROFILE)
     {
         Write-Verbose "$PROFILE found"
-        Move-Item -Path $PROFILE -Destination "$PROFILE.old"
-        Write-Verbose "Moved $PROFILE to $PROFILE.old"
+        Move-Item -Path $PROFILE -Destination "$($PROFILE).old"
+        Write-Verbose "Moved $PROFILE to $($PROFILE).old"
+    }
+    else
+    {
+        Write-Verbose "$PROFILE not found"
     }
 
-    # Creating a symlink to the new Profile
-    New-Item -Name $PROFILE -ItemType SymbolicLink -Target .\Microsoft.PowerShell_profile.ps1
-    Write-Verbose "Created symlink at $PROFILE"
-
-    # Installing OhMyPosh for the prompt style
-    winget install --id JanDeDobbeleer.OhMyPosh -e
-
-    # Installing Terminal Icons
-    Install-Module -Name Terminal-Icons -Repository PSGallery
+    # Windows PowerShell or PowerShell core
+    switch ($PSVersionTable.PSEdition)
+    {
+        "Core"
+        { 
+            Write-Verbose "PowerShell core version"
+            $profilePath = "PowerShell"
+        }
+        "Desktop"
+        {
+            Write-Verbose "Windows Powershell version"
+            $profilePath = "WindowsPowerShell"
+        }
+        Default
+        {
+            Write-Host "Not on windows" -ForegroundColor Red
+            $profilePath = $null
+        }
+    }
+    
+    # Creating a symlink to the new Profile if the path is not empty
+    if ($null -ne $profilePath)
+    {   
+        # Checking if it's the VSCode Powershell extension profile
+        if ($PROFILE.Contains("VSCode"))
+        {
+            $profileFile = "Microsoft.VSCode_profile.ps1"
+        }
+        
+        New-Item -ItemType SymbolicLink -Name $profileFile -Path "$($env:USERPROFILE)\Documents\$($profilePath)" -Target "$($PWD)\Microsoft.PowerShell_profile.ps1" -ErrorAction Stop
+        Write-Verbose "Created symlink at $PROFILE"
+    }  
 }
 catch
 {
     Write-Host $_.Exception.Message -ForegroundColor Red
 }
+
+try
+{
+    # Installing OhMyPosh for the prompt style
+    winget install -e -s winget --id JanDeDobbeleer.OhMyPosh
+    Write-Verbose "Installed OhMyPosh"
+
+    # Installing Terminal Icons
+    Install-Module -Name Terminal-Icons -Repository PSGallery
+    Write-Verbose "Installed Terminal-Icons"
+}
+catch {
+    Write-Host $_.Exception.Message -ForegroundColor Red
+}
+
+# Refresh the Profile
+& $PROFILE
